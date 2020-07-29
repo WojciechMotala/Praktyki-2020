f47142f8cc114040391b15ee45f66d61a7db03c0
WojciechMotala <wojciech.motala@gamil.com>
2020-07-29 09:41:40 +0200

    dodanie pliku z testowa funkcja kalmana

---
+++ src/kalmantest.h
@@ -0,0 +1,120 @@
+﻿#pragma once
+
+using namespace std;
+
+struct Trajectory
+{
+	Trajectory() {}
+	Trajectory(double _x, double _y, double _a) {
+		x = _x;
+		y = _y;
+		a = _a;
+	}
+	// "+"
+	friend Trajectory operator+(const Trajectory& c1, const Trajectory& c2) {
+		return Trajectory(c1.x + c2.x, c1.y + c2.y, c1.a + c2.a);
+	}
+	//"-"
+	friend Trajectory operator-(const Trajectory& c1, const Trajectory& c2) {
+		return Trajectory(c1.x - c2.x, c1.y - c2.y, c1.a - c2.a);
+	}
+	//"*"
+	friend Trajectory operator*(const Trajectory& c1, const Trajectory& c2) {
+		return Trajectory(c1.x * c2.x, c1.y * c2.y, c1.a * c2.a);
+	}
+	//"/"
+	friend Trajectory operator/(const Trajectory& c1, const Trajectory& c2) {
+		return Trajectory(c1.x / c2.x, c1.y / c2.y, c1.a / c2.a);
+	}
+	//"="
+	Trajectory operator =(const Trajectory& rx) {
+		x = rx.x;
+		y = rx.y;
+		a = rx.a;
+		return Trajectory(x, y, a);
+	}
+
+	double x;
+	double y;
+	double a; // angle
+};
+
+vector<Matrix3d> kTest(vector<Matrix3d> H) {
+
+	// Accumulated frame to frame transform
+	double a = 0;
+	double x = 0;
+	double y = 0;
+
+	vector <Trajectory> smoothed_trajectory; // trajectory at all frames
+	Trajectory X;//posteriori state estimate
+	Trajectory	X_;//priori estimate
+	Trajectory P;// posteriori estimate error covariance
+	Trajectory P_;// priori estimate error covariance
+	Trajectory K;//gain
+	Trajectory	z;//actual measurement
+	double pstd = 4e-3;//can be changed
+	double cstd = 0.25;//can be changed
+	Trajectory Q(pstd, pstd, pstd);// process noise covariance
+	Trajectory R(cstd, cstd, cstd);// measurement noise covariance 
+
+	int k = 1;
+
+	vector<Matrix3d> result;
+
+	for (int i = 0; i < H.size(); i++) {
+
+		// decompose T
+		double dx = H[i](0, 2);
+		double dy = H[i](1, 2);
+		double da = atan2(H[i](1, 0), H[i](0, 0));
+
+		x += dx;
+		y += dy;
+		a += da;
+
+		z = Trajectory(x, y, a);
+
+		if (k == 1) {
+			// intial guesses
+			X = Trajectory(0, 0, 0); //Initial estimate,  set 0
+			P = Trajectory(1, 1, 1); //set error variance,set 1
+		}
+		else
+		{
+			//time update（prediction）
+			X_ = X; //X_(k) = X(k-1);
+			P_ = P + Q; //P_(k) = P(k-1)+Q;
+			// measurement update（correction）
+			K = P_ / (P_ + R); //gain;K(k) = P_(k)/( P_(k)+R );
+			X = X_ + K * (z - X_); //z-X_ is residual,X(k) = X_(k)+K(k)*(z(k)-X_(k)); 
+			P = (Trajectory(1, 1, 1) - K) * P_; //P(k) = (1-K(k))*P_(k);
+		}
+
+		// target - current
+		double diff_x = X.x - x;//
+		double diff_y = X.y - y;
+		double diff_a = X.a - a;
+
+		dx = dx + diff_x;
+		dy = dy + diff_y;
+		da = da + diff_a;
+
+		Matrix3d outH;
+		outH(0, 0) = cos(da);
+		outH(0, 1) = -sin(da);
+		outH(0, 2) = dx;
+		outH(1, 0) = sin(da);
+		outH(1, 1) = cos(da);
+		outH(1, 2) = dy;
+		outH(2, 0) = 0.0;
+		outH(2, 1) = 0.0;
+		outH(2, 2) = 1.0;
+
+		result.push_back(outH);
+
+		k++;
+	}
+
+	return result;
+}
